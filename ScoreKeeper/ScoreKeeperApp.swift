f06e6b296c43e7517b098fb2cdf8a4ea184fae2a
Pablo Porzio <pabloporzio97@gmail.com>
2024-12-04 14:03:58 -0300

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Pablo Porzio (Modak) on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
